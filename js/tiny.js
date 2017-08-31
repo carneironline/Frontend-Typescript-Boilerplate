07d94deb1293d4b9f75e7249e169ae3c74a0c441
rmcorrea <raphael.correa@infoglobo.com.br>
2017-08-31 14:59:33 -0300

    RD-3278 - Ad Blocker
    Eu, como Produto, quero poder configurar experiências na Piano que detectem uso de ADBLOCK

--- js/tiny.js
+++ js/tiny.js
@@ -435,6 +435,19 @@ Piano.util = {
 		}
 		return false;
 	},
+	detectaAdBlock: function() {
+		document.cookie = "__adblocker=; expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/";
+		var setNptTechAdblockerCookie = function(adblocker) {
+			var d = new Date();
+			d.setTime(d.getTime() + 60 * 60 * 24 * 2 * 1000);
+			document.cookie = "__adblocker=" + (adblocker ? "true" : "false") + "; expires=" + d.toUTCString() + "; path=/";
+		}
+		var script = document.createElement("script");
+		script.setAttribute("async", true);
+		script.setAttribute("src", "//www.npttech.com/advertising.js");
+		script.setAttribute("onerror", "setNptTechAdblockerCookie(true);");
+		document.getElementsByTagName("head")[0].appendChild(script);
+	},
 	callbackMeter: function(meterData) {
 		regrasTiny = meterData;
 		Piano.metricas.executaAposPageview();
@@ -475,6 +488,7 @@ Piano.construtor = {
 };
 
 (function () {
+	Piano.util.detectaAdBlock();
 	Piano.construtor.initTp();
 })();
 
