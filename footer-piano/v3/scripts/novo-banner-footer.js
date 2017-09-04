511d766e0153b3839f0199492e46922fd7b433fd
Vinicius Loureiro <carneiropresent@gmail.com>
2017-09-04 17:18:28 -0300

    novo footer editavel

--- footer-piano/v3/scripts/novo-banner-footer.js
+++ footer-piano/v3/scripts/novo-banner-footer.js
@@ -5,63 +5,63 @@ $("body").append("" +
 "           <div class='produtos-oglobo'>" +
 "               <div class='container-conteudos'>" +
 "                   <ul class='lista-de-produtos-oglobo'>" +
-"                       <li class='produto-digital'>" +
+"                       <li class='produto-um'>" +
 "                           <ul>" +
 "                               <li class='img-produto'>" +
 "                                   <img alt='pacote 1' src='https://static"+Piano.util.montaUrlStg()+".infoglobo.com.br/paywall/footer-piano/v2/images/digital_pack_02.png'>" +
 "                               </li>" +
 "                               <li class='titulo-produto'>" +
 "                                   <div class='oglobo-logo'></div>" +
-"                                   <h2>Digital</h2>" +
-"                                   <h3>A informação na medida" +
+"                                   <h2 class='azul-medio'>Digital</h2>" +
+"                                   <h3 class='compartilha-cor'>A informação na medida" +
 "                                       <br/> certa para você." +
 "                                   </h3>" +
-"                                   <a target='_blank' href='#'>" +
+"                                   <a class='azul-medio' target='_blank' href='#'>" +
 "                                       <u>Saiba mais</u> ›" +
 "                                   </a>" +
 "                               </li>" +
-"                               <li class='informacao-produto'>Site " +
+"                               <li class='informacao-produto compartilha-cor'>Site " +
 "                                   <br/>App O Globo Notícias" +
 "                               </li>" +
 "                               <li class='preco-produto'>" +
-"                                   <del>R$ 9,90</del>" +
-"                                   <strong>R$ 1,90</strong>" +
-"                                   <small>" +
-"                                       <span>/ MÊS</span>" +
-"                                       <b>no primeiro mês</b>" +
+"                                   <del class='compartilha-cor'>R$ 9,90</del>" +
+"                                   <strong class='compartilha-cor'>R$ 1,90</strong>" +
+"                                   <small class='compartilha-cor'>" +
+"                                       <span class='azul-medio'>/ MÊS</span>" +
+"                                       <b>no 1° mês</b>" +
 "                                   </small>" +
-"                                   <a target='_blank' href='#'>Assine já</a>" +
+"                                   <a class='btn-default' target='_blank' href='#'>Assine já</a>" +
 "                               </li>" +
 "                           </ul>" +
 "                       </li>" +
-"                       <li class='produto-digital-premium'>" +
+"                       <li class='produto-dois'>" +
 "                           <ul>" +
 "                               <li class='img-produto'>" +
 "                                   <img alt='pacote 2' src='https://static"+Piano.util.montaUrlStg()+".infoglobo.com.br/paywall/footer-piano/v2/images/digital.png'>" +
 "                                   </li>" +
 "                               <li class='titulo-produto'>" +
 "                                   <div class='oglobo-logo'></div>" +
-"                                   <h2>Digital Premium</h2>" +
-"                                   <h3>Acesso ilimitado a todos os produtos digitais do Globo.</h3>" +
-"                                   <a target='_blank' href='#'>" +
+"                                   <h2 class='azul-medio'>Digital Premium</h2>" +
+"                                   <h3 class='compartilha-cor'>Acesso ilimitado a todos os produtos digitais do Globo.</h3>" +
+"                                   <a class='azul-medio' target='_blank' href='#'>" +
 "                                       <u>Saiba mais</u> ›" +
 "                                   </a>" +
 "                               </li>" +
-"                               <li class='informacao-produto'>Site " +
+"                               <li class='informacao-produto compartilha-cor'>Site " +
 "                                   <br/>App O Globo Notícias" +
 "                                   <br/>App Globo Mais" +
 "                                   <br/>Jornal Digital" +
-"                                   <br/>Acervo" +
+"                                   <br/>Acesso ilimitado ao Site" +
 "                                   <br/>Clube O Globo Sou+Rio" +
 "                               </li>" +
 "                               <li class='preco-produto'>" +
-"                                   <del>R$ 29,90</del>" +
-"                                   <strong>R$ 5,90</strong>" +
-"                                   <small>" +
-"                                       <span>/ MÊS</span>" +
+"                                   <del class='compartilha-cor'>R$ 29,90</del>" +
+"                                   <strong class='compartilha-cor'>R$ 5,90</strong>" +
+"                                   <small class='compartilha-cor'>" +
+"                                       <span class='azul-medio'>/ MÊS</span>" +
 "                                       <b>por 3 meses</b>" +
 "                                   </small>" +
-"                                   <a target='_blank' href='#'>Assine já</a>" +
+"                                   <a class='btn-default' target='_blank' href='#'>Assine já</a>" +
 "                               </li>" +
 "                           </ul>" +
 "                       </li>" +
@@ -70,13 +70,13 @@ $("body").append("" +
 "               </div>" +
 "               <div class='chamada-fixa'>" +
 "                   <div class='logo-slogan'></div>" +
-"                   <a class='chamada-telefone'>Televendas: 4002 5300 (capitais e grandes cidades) / 0800 021 8433 (demais localidades)</a>" +
-"                   <h2>" +
+"                   <a class='chamada-telefone'><small>Televendas: 4002 5300 (capitais e grandes cidades) 0800 021 8433 (demais localidades)</small></a>" +
+"                   <h2 class='compartilha-cor'>" +
 "                       <span class='assine-comeco'>Assine por apenas</span>" +
-"                       <strong>R$ 1,90</strong>" +
+"                       <strong class='azul-medio'>R$ 1,90</strong>" +
 "                       <span class='assine-fim'>no 1º mês.<span>" +
 "                   </h2>" +
-"                   <span class='chamada-abre-oferta'>Ver ofertas</span>" +
+"                   <span class='chamada-abre-oferta btn-default'>Ver ofertas</span>" +
 "               </div>" +
 "           </div>" +
 "       </div>" +
@@ -94,14 +94,14 @@ var a=$(".footer-venda-oglobo-capas");
 a.toggleClass("open"),
 a.is(".open")?EventoGaPiano.disparaEvento(!0,"Banner Footer","Clique","Expandir",!1):EventoGaPiano.disparaEvento(!0,"Banner Footer","Clique","Fechar",!1)}),
 
-$(".footer-venda-oglobo-capas .produto-digital .preco-produto a").click(function(a){a.preventDefault(),
-	window.open($(".footer-venda-oglobo-capas .produto-digital .preco-produto a").attr("href")),EventoGaPiano.disparaEvento(!0,"Banner Footer","Clique","Assine (link para pagamento) - Light",!1)}),
-$(".footer-venda-oglobo-capas .produto-digital-premium .preco-produto a").click(function(a){a.preventDefault(),
-	window.open($(".footer-venda-oglobo-capas .produto-digital-premium .preco-produto a").attr("href")),EventoGaPiano.disparaEvento(!0,"Banner Footer","Clique","Assine (link para pagamento) - Digital",!1)}),
-$(".footer-venda-oglobo-capas .produto-digital .titulo-produto a").click(function(a){a.preventDefault(),
-	window.open($(".footer-venda-oglobo-capas .produto-digital .titulo-produto a").attr("href")),EventoGaPiano.disparaEvento(!0, "Banner Footer","Clique","Saiba mais - Light",!1)}),
-$(".footer-venda-oglobo-capas .produto-digital-premium .titulo-produto a").click(function(a){a.preventDefault(),
-	window.open($(".footer-venda-oglobo-capas .produto-digital-premium .titulo-produto a").attr("href")),EventoGaPiano.disparaEvento(!0,"Banner Footer","Clique","Saiba mais - Digital",!1)});
+$(".footer-venda-oglobo-capas .produto-um .preco-produto a").click(function(a){a.preventDefault(),
+	window.open($(".footer-venda-oglobo-capas .produto-um .preco-produto a").attr("href")),EventoGaPiano.disparaEvento(!0,"Banner Footer","Clique","Assine (link para pagamento) - Light",!1)}),
+$(".footer-venda-oglobo-capas .produto-dois .preco-produto a").click(function(a){a.preventDefault(),
+	window.open($(".footer-venda-oglobo-capas .produto-dois .preco-produto a").attr("href")),EventoGaPiano.disparaEvento(!0,"Banner Footer","Clique","Assine (link para pagamento) - Digital",!1)}),
+$(".footer-venda-oglobo-capas .produto-um .titulo-produto a").click(function(a){a.preventDefault(),
+	window.open($(".footer-venda-oglobo-capas .produto-um .titulo-produto a").attr("href")),EventoGaPiano.disparaEvento(!0, "Banner Footer","Clique","Saiba mais - Light",!1)}),
+$(".footer-venda-oglobo-capas .produto-dois .titulo-produto a").click(function(a){a.preventDefault(),
+	window.open($(".footer-venda-oglobo-capas .produto-dois .titulo-produto a").attr("href")),EventoGaPiano.disparaEvento(!0,"Banner Footer","Clique","Saiba mais - Digital",!1)});
 
 if (typeof preco0 != 'undefined' && preco0 != null && preco0 != '' ) {
 	$(".chamada-fixa h2 strong").text(preco0);
@@ -120,101 +120,147 @@ if (typeof abrirBanner != 'undefined' && abrirBanner != null && abrirBanner != '
 }
 
 if (typeof preco1 != 'undefined' && preco1 != null && preco1 != '' ) {
-	$(".produto-digital .preco-produto strong").text(preco1);
+	$(".produto-um .preco-produto strong").text(preco1);
 }
 
 if (typeof preco2 != 'undefined' && preco2 != null && preco2 != '' ) {
-	$(".produto-digital-premium .preco-produto strong").text(preco2);
+	$(".produto-dois .preco-produto strong").text(preco2);
 }
 
 if (typeof precoBanca1 != 'undefined' && precoBanca1 != null && precoBanca1 != '' ) {
-	$(".produto-digital .preco-produto del").text(precoBanca1);
+	$(".produto-um .preco-produto del").text(precoBanca1);
 }
 
 if (typeof precoBanca2 != 'undefined' && precoBanca2 != null && precoBanca2 != '' ) {
-	$(".produto-digital-premium .preco-produto del").text(precoBanca2);
+	$(".produto-dois .preco-produto del").text(precoBanca2);
 }
 
 if (typeof urlImg1 != 'undefined' && urlImg1 != null && urlImg1 != '' ) {
-	$(".produto-digital .img-produto img").attr("src", urlImg1);
+	$(".produto-um .img-produto img").attr("src", urlImg1);
 }
 
 if (typeof urlImg2 != 'undefined' && urlImg2 != null && urlImg2 != '' ) {
-	$(".produto-digital-premium .img-produto img").attr("src", urlImg2);
+	$(".produto-dois .img-produto img").attr("src", urlImg2);
 }
 
 if (typeof linkInfo1 != 'undefined' && linkInfo1 != null && linkInfo1 != '' ) {
-	$(".produto-digital .titulo-produto a").attr("href", linkInfo1);
+	$(".produto-um .titulo-produto a").attr("href", linkInfo1);
 }
 
 if (typeof linkInfo2 != 'undefined' && linkInfo2 != null && linkInfo2 != '' ) {
-	$(".produto-digital-premium .titulo-produto a").attr("href", linkInfo2);
+	$(".produto-dois .titulo-produto a").attr("href", linkInfo2);
 }
 
 if (typeof linkVenda1 != 'undefined' && linkVenda1 != null && linkVenda1 != '' ) {
-	$(".produto-digital .preco-produto a").attr("href", linkVenda1);
+	$(".produto-um .preco-produto a").attr("href", linkVenda1);
 }
 
 if (typeof linkVenda2 != 'undefined' && linkVenda2 != null && linkVenda2 != '' ) {
-	$(".produto-digital-premium .preco-produto a").attr("href", linkVenda2);
+	$(".produto-dois .preco-produto a").attr("href", linkVenda2);
 }
 
 if (typeof textoBtnAssine1 != 'undefined' && textoBtnAssine1 != null && textoBtnAssine1 != '' ) {
-	$(".produto-digital .preco-produto a").text(textoBtnAssine1);
+	$(".produto-um .preco-produto a").text(textoBtnAssine1);
 }
 
 if (typeof textoBtnAssine2 != 'undefined' && textoBtnAssine2 != null && textoBtnAssine2 != '' ) {
-	$(".produto-digital-premium .preco-produto a").text(textoBtnAssine2);
+	$(".produto-dois .preco-produto a").text(textoBtnAssine2);
 }
 
 if (typeof telefone != 'undefined' && telefone != null && telefone != '' ) {
-	$(".chamada-fixa .chamada-telefone").text(telefone);
+	$(".chamada-fixa .chamada-telefone").html(telefone);
 }
 
 if (typeof quantDesconto1 != 'undefined' && quantDesconto1 != null && quantDesconto1 != '' ) {
-	$(".produto-digital .preco-produto small span").text(quantDesconto1);
+	$(".produto-um .preco-produto small span").text(quantDesconto1);
 }
 
 if (typeof quantTempoDec1 != 'undefined' && quantTempoDec1 != null && quantTempoDec1 != '' ) {
-	$(".produto-digital .preco-produto small b").text(quantTempoDec1);
+	$(".produto-um .preco-produto small b").text(quantTempoDec1);
 }
 
 if (typeof quantDesconto2 != 'undefined' && quantDesconto2 != null && quantDesconto2 != '' ) {
-	$(".produto-digital-premium .preco-produto small span").text(quantDesconto2);
+	$(".produto-dois .preco-produto small span").text(quantDesconto2);
 }
 
 if (typeof quantTempoDec2 != 'undefined' && quantTempoDec2 != null && quantTempoDec2 != '' ) {
-	$(".produto-digital-premium .preco-produto small b").text(quantTempoDec2);
+	$(".produto-dois .preco-produto small b").text(quantTempoDec2);
 }
 
 if (typeof nomePacote1 != 'undefined' && nomePacote1 != null && nomePacote1 != '' ) {
-	$(".produto-digital .titulo-produto h2").text(nomePacote1);
+	$(".produto-um .titulo-produto h2").text(nomePacote1);
 }
 
 if (typeof nomePacote2 != 'undefined' && nomePacote2 != null && nomePacote2 != '' ) {
-	$(".produto-digital-premium .titulo-produto h2").text(nomePacote2);
+	$(".produto-dois .titulo-produto h2").text(nomePacote2);
 }
 
 if (typeof descricaoPacote1 != 'undefined' && descricaoPacote1 != null && descricaoPacote1 != '' ) {
-	$(".produto-digital .titulo-produto h3").text(descricaoPacote1);
+	$(".produto-um .titulo-produto h3").text(descricaoPacote1);
 }
 
 if (typeof descricaoPacote2 != 'undefined' && descricaoPacote2 != null && descricaoPacote2 != '' ) {
-	$(".produto-digital-premium .titulo-produto h3").text(descricaoPacote2);
+	$(".produto-dois .titulo-produto h3").text(descricaoPacote2);
 }
 
 if (typeof textoLinkSaibaMais1 != 'undefined' && textoLinkSaibaMais1 != null && textoLinkSaibaMais1 != '' ) {
-	$(".produto-digital .titulo-produto a u").text(textoLinkSaibaMais1);
+	$(".produto-um .titulo-produto a u").text(textoLinkSaibaMais1);
 }
 
 if (typeof textoLinkSaibaMais2 != 'undefined' && textoLinkSaibaMais2 != null && textoLinkSaibaMais2 != '' ) {
-	$(".produto-digital-premium .titulo-produto a u").text(textoLinkSaibaMais2);
+	$(".produto-dois .titulo-produto a u").text(textoLinkSaibaMais2);
 }
 
 if (typeof listaProdutos1 != 'undefined' && listaProdutos1 != null && listaProdutos1 != '' ) {
-	$(".produto-digital .informacao-produto").html(listaProdutos1);
+	$(".produto-um .informacao-produto").html(listaProdutos1);
 }
 
 if (typeof listaProdutos2 != 'undefined' && listaProdutos2 != null && listaProdutos2 != '' ) {
-	$(".produto-digital-premium .informacao-produto").html(listaProdutos2);
+	$(".produto-dois .informacao-produto").html(listaProdutos2);
 }
+
+if (typeof background1 == 'undefined' || background1 == null || background1 == '' ) {
+	background1 = "#1d4c9c";
+}
+
+if (typeof background2 == 'undefined' || background2 == null || background2 == '' ) {
+	background2 = "#fff";
+}
+
+if (typeof backgroundBotao == 'undefined' || backgroundBotao == null || backgroundBotao == '' ) {
+	backgroundBotao = "#00aeef";
+}
+
+if (typeof backgroundBotaoHover == 'undefined' || backgroundBotaoHover == null || backgroundBotaoHover == '' ) {
+	backgroundBotaoHover = "#73d9ff";
+}
+
+if (typeof corTelefone == 'undefined' || corTelefone == null || corTelefone == '' ) {
+	corTelefone = "#bdbec1";
+}
+
+if (typeof corTextoBotao == 'undefined' || corTextoBotao == null || corTextoBotao == '' ) {
+	corTextoBotao = "#fff";
+}
+
+if (typeof corPadrao == 'undefined' || corPadrao == null || corPadrao == '' ) {
+	corPadrao = "#fff";
+}
+
+if (typeof corDestaque == 'undefined' || corDestaque == null || corDestaque == '' ) {
+	corDestaque = "#139deb";
+}
+
+
+$("head").append("" +
+"<style>" +
+".footer-venda-oglobo-capas .produtos-oglobo .container-conteudos {background-color: "+background1+";}" +
+".footer-venda-oglobo-capas .produtos-oglobo .chamada-fixa {background-color: "+background1+";}" +
+".footer-venda-oglobo-capas.open .produtos-oglobo .chamada-fixa {background-color: "+background2+";}" +
+".footer-venda-oglobo-capas .produtos-oglobo .chamada-fixa .chamada-telefone {color: "+corTelefone+";}" +
+".footer-venda-oglobo-capas .btn-default{background-color: "+backgroundBotao+"; color: "+corTextoBotao+";}" +
+".footer-venda-oglobo-capas .btn-default:hover{background-color: "+backgroundBotaoHover+";}" +
+".footer-venda-oglobo-capas .azul-medio {color: "+corDestaque+"}" +
+".footer-venda-oglobo-capas .compartilha-cor {color: "+corPadrao+";}" +
+".footer-venda-oglobo-capas {opacity: 1;}" +
+"</style>");
