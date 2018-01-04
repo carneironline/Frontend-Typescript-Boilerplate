05b5aab0f756c12ce65f29e25c52f48d54bc7533
vinnycl <carneiropresent@gmail.com>
2018-01-04 11:58:35 -0200

    Migracao do jQuery no Banner Header / Footer

--- banner-header-footer-piano/v1/scripts/banner-header-footer-piano.js
+++ banner-header-footer-piano/v1/scripts/banner-header-footer-piano.js
@@ -1,11 +1,11 @@
 if (window.linkAssinaturaHeaderFooter) {
-	$("footer .footer #banner-assinatura-footer a, .top-header ul.menu-interacao>li#banner-assinatura a, .top-header ul.menu-interacao > li#banner-assinatura a, footer .bar .assine a").attr("href", linkAssinaturaHeaderFooter);
+	document.querySelector("footer .footer #banner-assinatura-footer a, .top-header ul.menu-interacao>li#banner-assinatura a, .top-header ul.menu-interacao > li#banner-assinatura a, footer .bar .assine a").href = linkAssinaturaHeaderFooter;
 } else {
-    $("footer .footer #banner-assinatura-footer a, .top-header ul.menu-interacao>li#banner-assinatura a, .top-header ul.menu-interacao > li#banner-assinatura a, footer .bar .assine a").attr("href", "https://seguro.oglobo.com.br/assinatura-jornal-oglobo/assine/landing-siteoglobo/?utm_source=siteoglobo&utm_medium=homeeinternas&utm_campaign=botaoassine_topo_rodape");
+    document.querySelector("footer .footer #banner-assinatura-footer a, .top-header ul.menu-interacao>li#banner-assinatura a, .top-header ul.menu-interacao > li#banner-assinatura a, footer .bar .assine a").href = "https://seguro.oglobo.com.br/assinatura-jornal-oglobo/assine/landing-siteoglobo/?utm_source=siteoglobo&utm_medium=homeeinternas&utm_campaign=botaoassine_topo_rodape";
 }
 if (window.imagemAssinaturaHeaderFooter) {    
-    $("footer .footer #banner-assinatura-footer a, .top-header ul.menu-interacao>li#banner-assinatura a, .top-header ul.menu-interacao > li#banner-assinatura a, footer .bar .assine a").css("background-image", "url("+imagemAssinaturaHeaderFooter+")");
+    document.querySelector("footer .footer #banner-assinatura-footer a, .top-header ul.menu-interacao>li#banner-assinatura a, .top-header ul.menu-interacao > li#banner-assinatura a, footer .bar .assine a").style.backgroundImage = "url("+imagemAssinaturaHeaderFooter+")";
 } else {
     imagemAssinaturaHeaderFooter = "https://static"+Piano.util.montaUrlStg()+".infoglobo.com.br/paywall/banner-header-footer-piano/v1/images/btn-01.png"
-    $("footer .footer #banner-assinatura-footer a, .top-header ul.menu-interacao>li#banner-assinatura a, .top-header ul.menu-interacao > li#banner-assinatura a, footer .bar .assine a").css("background-image", "url("+imagemAssinaturaHeaderFooter+")");
+    document.querySelector("footer .footer #banner-assinatura-footer a, .top-header ul.menu-interacao>li#banner-assinatura a, .top-header ul.menu-interacao > li#banner-assinatura a, footer .bar .assine a").style.backgroundImage = "url("+imagemAssinaturaHeaderFooter+")";
 }
