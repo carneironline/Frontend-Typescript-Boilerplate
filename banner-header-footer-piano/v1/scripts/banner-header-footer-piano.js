f10b68ab9badb8601f51d50371776781d9197781
vinnycl <carneiropresent@gmail.com>
2018-01-04 17:27:05 -0200

    Removendo jQuery - Selecionar vários seletores com o mesmo atributo

--- banner-header-footer-piano/v1/scripts/banner-header-footer-piano.js
+++ banner-header-footer-piano/v1/scripts/banner-header-footer-piano.js
@@ -1,11 +1,22 @@
+var x = document.querySelectorAll("footer .footer #banner-assinatura-footer a, .top-header ul.menu-interacao>li#banner-assinatura a, .top-header ul.menu-interacao > li#banner-assinatura a, footer .bar .assine a");
+var i;
 if (window.linkAssinaturaHeaderFooter) {
-	document.querySelector("footer .footer #banner-assinatura-footer a, .top-header ul.menu-interacao>li#banner-assinatura a, .top-header ul.menu-interacao > li#banner-assinatura a, footer .bar .assine a").href = linkAssinaturaHeaderFooter;
+	for (i = 0; i < x.length; i++) {
+		x[i].href = linkAssinaturaHeaderFooter;
+	}
 } else {
-    document.querySelector("footer .footer #banner-assinatura-footer a, .top-header ul.menu-interacao>li#banner-assinatura a, .top-header ul.menu-interacao > li#banner-assinatura a, footer .bar .assine a").href = "https://seguro.oglobo.com.br/assinatura-jornal-oglobo/assine/landing-siteoglobo/?utm_source=siteoglobo&utm_medium=homeeinternas&utm_campaign=botaoassine_topo_rodape";
+	for (i = 0; i < x.length; i++) {
+		x[i].href = "https://seguro.oglobo.com.br/assinatura-jornal-oglobo/assine/landing-siteoglobo/?utm_source=siteoglobo&utm_medium=homeeinternas&utm_campaign=botaoassine_topo_rodape";
+	}
 }
-if (window.imagemAssinaturaHeaderFooter) {    
-    document.querySelector("footer .footer #banner-assinatura-footer a, .top-header ul.menu-interacao>li#banner-assinatura a, .top-header ul.menu-interacao > li#banner-assinatura a, footer .bar .assine a").style.backgroundImage = "url("+imagemAssinaturaHeaderFooter+")";
+
+if (window.imagemAssinaturaHeaderFooter) {
+	for (i = 0; i < x.length; i++) {
+		x[i].style.backgroundImage = "url("+imagemAssinaturaHeaderFooter+")";
+	}
 } else {
-    imagemAssinaturaHeaderFooter = "https://static"+Piano.util.montaUrlStg()+".infoglobo.com.br/paywall/banner-header-footer-piano/v1/images/btn-01.png"
-    document.querySelector("footer .footer #banner-assinatura-footer a, .top-header ul.menu-interacao>li#banner-assinatura a, .top-header ul.menu-interacao > li#banner-assinatura a, footer .bar .assine a").style.backgroundImage = "url("+imagemAssinaturaHeaderFooter+")";
+	imagemAssinaturaHeaderFooter = "https://static"+Piano.util.montaUrlStg()+".infoglobo.com.br/paywall/banner-header-footer-piano/v1/images/btn-01.png"
+	for (i = 0; i < x.length; i++) {
+		x[i].style.backgroundImage = "url("+imagemAssinaturaHeaderFooter+")";
+	}
 }
