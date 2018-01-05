80917de267195e8461bdfb22a247a17ea2fb1d50
vinnycl <carneiropresent@gmail.com>
2018-01-05 13:42:26 -0200

    Diminuir loops para o Header/Footer

--- banner-header-footer-piano/v1/scripts/banner-header-footer-piano.js
+++ banner-header-footer-piano/v1/scripts/banner-header-footer-piano.js
@@ -1,22 +1,15 @@
 var x = document.querySelectorAll("footer .footer #banner-assinatura-footer a, .top-header ul.menu-interacao>li#banner-assinatura a, .top-header ul.menu-interacao > li#banner-assinatura a, footer .bar .assine a");
 var i;
-if (window.linkAssinaturaHeaderFooter) {
-	for (i = 0; i < x.length; i++) {
-		x[i].href = linkAssinaturaHeaderFooter;
+for (i = 0; i < x.length; i++) {
+	if (window.linkAssinaturaHeaderFooter) {		
+			x[i].href = linkAssinaturaHeaderFooter;
+	} else {
+			x[i].href = "https://seguro.oglobo.com.br/assinatura-jornal-oglobo/assine/landing-siteoglobo/?utm_source=siteoglobo&utm_medium=homeeinternas&utm_campaign=botaoassine_topo_rodape";
 	}
-} else {
-	for (i = 0; i < x.length; i++) {
-		x[i].href = "https://seguro.oglobo.com.br/assinatura-jornal-oglobo/assine/landing-siteoglobo/?utm_source=siteoglobo&utm_medium=homeeinternas&utm_campaign=botaoassine_topo_rodape";
-	}
-}
-
-if (window.imagemAssinaturaHeaderFooter) {
-	for (i = 0; i < x.length; i++) {
-		x[i].style.backgroundImage = "url("+imagemAssinaturaHeaderFooter+")";
-	}
-} else {
-	imagemAssinaturaHeaderFooter = "https://static"+Piano.util.montaUrlStg()+".infoglobo.com.br/paywall/banner-header-footer-piano/v1/images/btn-01.png"
-	for (i = 0; i < x.length; i++) {
-		x[i].style.backgroundImage = "url("+imagemAssinaturaHeaderFooter+")";
+	if (window.imagemAssinaturaHeaderFooter) {
+			x[i].style.backgroundImage = "url("+imagemAssinaturaHeaderFooter+")";
+	} else {
+		imagemAssinaturaHeaderFooter = "https://static"+Piano.util.montaUrlStg()+".infoglobo.com.br/paywall/banner-header-footer-piano/v1/images/btn-01.png"
+			x[i].style.backgroundImage = "url("+imagemAssinaturaHeaderFooter+")";		
 	}
 }
