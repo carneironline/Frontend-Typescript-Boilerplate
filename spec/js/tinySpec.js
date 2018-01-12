b9a736ea4f8a3e4a7abc0f0430f60741c8bed590
Lucas Gigek <lucasgigek@gmail.com>
2018-01-12 15:53:57 -0200

    Finalização dos testes da função isAutorizado

--- spec/js/tinySpec.js
+++ spec/js/tinySpec.js
@@ -21,60 +21,63 @@ describe('Tiny JS', function () {
         });
 
         it('não deve autorizar quando autorizado tem valor "false", motivo não tem valor "indisponivel" e termoDeUso '
-          + 'tem valor "false"',function(){
-            expect(piano.autenticacao.isAutorizado(false, '', false, '')).toEqual(false);
-        });
+            + 'tem valor "false"', function () {
+                expect(piano.autenticacao.isAutorizado(false, '', false, '')).toEqual(false);
+            });
 
-        it('deve fazer requisição para o barramento quando autorizado tem valor "true" e hrefAssinaturaInadimplente possui algum valor', function () {
-            spyOn(piano.ajax, 'fazRequisicaoBarramentoApiObterAssinaturaInadimplente');
+        it('deve fazer requisição para o barramento quando autorizado tem valor "true" e hrefAssinaturaInadimplente '
+            + 'possui algum valor', function () {
+                spyOn(piano.ajax, 'fazRequisicaoBarramentoApiObterAssinaturaInadimplente');
 
-            piano.autenticacao.isAutorizado(false, '', true, 'asd');
-            expect(piano.ajax.fazRequisicaoBarramentoApiObterAssinaturaInadimplente).toHaveBeenCalled();
-        });
+                piano.autenticacao.isAutorizado(false, '', true, 'asd');
+                expect(piano.ajax.fazRequisicaoBarramentoApiObterAssinaturaInadimplente).toHaveBeenCalled();
+            });
 
-        it('não deve fazer requisição para o barramento quando autorizado tem valor "true" e hrefAssinaturaInadimplente não possui valor', function () {
-            spyOn(piano.ajax, 'fazRequisicaoBarramentoApiObterAssinaturaInadimplente');
+        it('não deve fazer requisição para o barramento quando autorizado tem valor "true" e hrefAssinaturaInadimplente '
+            + 'não possui valor', function () {
+                spyOn(piano.ajax, 'fazRequisicaoBarramentoApiObterAssinaturaInadimplente');
 
-            piano.autenticacao.isAutorizado(false, '', true, '');
-            expect(piano.ajax.fazRequisicaoBarramentoApiObterAssinaturaInadimplente).not.toHaveBeenCalled();
-        });
+                piano.autenticacao.isAutorizado(false, '', true, '');
+                expect(piano.ajax.fazRequisicaoBarramentoApiObterAssinaturaInadimplente).not.toHaveBeenCalled();
+            });
 
-        it('não deve fazer requisição para o barramento quando autorizado tem valor "false" e hrefAssinaturaInadimplente possui algum valor', function () {
-            spyOn(piano.ajax, 'fazRequisicaoBarramentoApiObterAssinaturaInadimplente');
+        it('não deve fazer requisição para o barramento quando autorizado tem valor "false" e hrefAssinaturaInadimplente '
+            + 'possui algum valor', function () {
+                spyOn(piano.ajax, 'fazRequisicaoBarramentoApiObterAssinaturaInadimplente');
 
-            piano.autenticacao.isAutorizado(false, '', false, 'asd');
-            expect(piano.ajax.fazRequisicaoBarramentoApiObterAssinaturaInadimplente).not.toHaveBeenCalled();
-        });
+                piano.autenticacao.isAutorizado(false, '', false, 'asd');
+                expect(piano.ajax.fazRequisicaoBarramentoApiObterAssinaturaInadimplente).not.toHaveBeenCalled();
+            });
 
-        it('deve chamar o método cookies.get se o usuário não for autorizado', function(){
+        it('deve chamar o método cookies.get se o usuário não for autorizado', function () {
             spyOn(piano.cookies, 'get');
 
             piano.autenticacao.isAutorizado(false, '', false, '');
             expect(piano.cookies.get).toHaveBeenCalled();
         });
 
-        it('não deve chamar o método cookies.get se o usuário for autorizado', function(){
+        it('não deve chamar o método cookies.get se o usuário for autorizado', function () {
             spyOn(piano.cookies, 'get');
 
             piano.autenticacao.isAutorizado(false, '', true, '');
             expect(piano.cookies.get).not.toHaveBeenCalled();
         });
 
-        // it('deve chamar o método cookies.set quando o usuário não for autorizado e possuir o cookie RTIEX', function(){
-            
-        //     var a = new Utils();
-        //     a.set('_rtiex', 'asd', 1);
-        //     // console.log(piano.cookies.get(piano.variaveis.constante.cookie.RTIEX));
-        //     // piano.cookies.set(piano.variaveis.constante.cookie.RTIEX, 'asd', 1);
-        //     console.log(piano.cookies.get(piano.variaveis.constante.cookie.RTIEX));
+        it('deve chamar o método cookies.set quando o usuário não for autorizado e possuir o cookie RTIEX', function () {
+            spyOn(piano.cookies, 'get').and.returnValue('asd');
+            spyOn(piano.cookies, 'set');
 
-        //     // spyOn(piano.cookies, 'set');
+            piano.autenticacao.isAutorizado(false, '', false, '');
+            expect(piano.cookies.set).toHaveBeenCalled();
+        });
 
-        //     // piano.autenticacao.isAutorizado(false, '', false, '');
-        //     // expect(piano.cookies.set).toHaveBeenCalled();
-        // });
+        it('não deve chamar o método cookies.set quando o usuário não for autorizado e não possuir o cookie RTIEX', function () {
+            spyOn(piano.cookies, 'set');
+
+            piano.autenticacao.isAutorizado(false, '', false, '');
+            expect(piano.cookies.set).not.toHaveBeenCalled();
+        });
 
     });
-
 
 })
