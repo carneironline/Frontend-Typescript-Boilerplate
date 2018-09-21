764058a7d99f872bee1ea8f6e2639db358075e98
vinnycl <carneiropresent@gmail.com>
2018-09-21 18:01:23 -0300

    Js ajust on single if

--- register-piano/v5/scripts/nova-tela-register.js
+++ register-piano/v5/scripts/nova-tela-register.js
@@ -19,7 +19,7 @@ if(protectedContentEl) {
 	"            <h2><strong>Muito bom ter você por aqui!</strong> Adoramos bons leitores.</h2>" +
 	"            <h3>Você atingiu o número máximo de leituras gratuitas este mês.</h3>" +
 	"            <a id='btnRegister' href='#' target='_blank'><strong>Cadastre-se</strong> para continuar.</a>" +
-	"            <p id='textLogin'>Já possui conta <a href='#' onclick='callLoginRegister();'>Globo.com</a>?</p>" +
+	"            <p class='textLogin' >Já possui conta <a href='#' onclick='callLoginRegister();'>Globo.com</a>?</p>" +
 	"        </div>" +
 	"        <div class='paywall--offer-link '>" +
 	"            <a id='offerLink' class='paywall--offer-image' target='_blank' href='#'>oferta</a>" +
@@ -37,13 +37,9 @@ if(protectedContentEl) {
 	protectedContentEl.setAttribute('data-content', 'removed');
 	protectedContentEl.innerHTML = protectedContent;
 
-	if (window.ambienteUtilizadoPiano == 'prd') {
-		document.querySelector("#iframeCadun").src = 'https://login.globo.com/login/'+Piano.variaveis.getServicoId()+'?url=https%3A%2F%2Fs3.glbimg.com%2Fv1%2FAUTH_65d1930a0bda476ba8d3c25c5371ec3f%2Fpiano%2Fhelper%2Fredirect.html%23'+uri;
-		document.querySelector("#btnRegister").href = 'https://login.globo.com/cadastro/'+Piano.variaveis.getServicoId()+'?url='+uri;
-	} else {
-		document.querySelector("#iframeCadun").src = 'https://login.qa.globoi.com/login/'+Piano.variaveis.getServicoId()+'?url=https%3A%2F%2Fs3.glbimg.com%2Fv1%2FAUTH_65d1930a0bda476ba8d3c25c5371ec3f%2Fpiano%2Fhelper%2Fredirect.html%23'+uri;
-		document.querySelector("#btnRegister").href = 'https://login.qa.globoi.com/cadastro/'+Piano.variaveis.getServicoId()+'?url='+uri;
-	}
+	var url = window.ambienteUtilizadoPiano == 'prd' ? 'https://login.globo.com/' : 'https://login.qa.globoi.com/';
+	document.querySelector("#iframeCadun").src = url + 'login/' + Piano.variaveis.getServicoId()+'?url=https%3A%2F%2Fs3.glbimg.com%2Fv1%2FAUTH_65d1930a0bda476ba8d3c25c5371ec3f%2Fpiano%2Fhelper%2Fredirect.html%23'+uri;
+	document.querySelector("#btnRegister").href = url + 'cadastro/' + Piano.variaveis.getServicoId()+'?url=https%3A%2F%2Fs3.glbimg.com%2Fv1%2FAUTH_65d1930a0bda476ba8d3c25c5371ec3f%2Fpiano%2Fhelper%2Fredirect.html%23'+uri;
 
 	if (typeof imgDesk != 'undefined' && imgDesk != null && imgDesk != '' && imgMob != 'undefined' && imgMob != null && imgMob != '' && imgTab != 'undefined' && imgTab != null && imgTab != '') {
 		if (window.screen.width < 600 ) {
@@ -74,7 +70,7 @@ if(protectedContentEl) {
 	}
 
 	if (typeof textLogin != 'undefined' && textLogin != null && textLogin != '') {
-		document.querySelector("#textLogin").innerHTML = textLogin;
+		document.querySelector(".textLogin").innerHTML = textLogin;
 	}
 	
 	document.onreadystatechange = function(){
