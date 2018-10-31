9c00581457c66a6fcf7981a7c4dc8fa489844f48
vinnycl <carneiropresent@gmail.com>
2018-10-31 14:10:26 -0300

    Remove test console

--- banner-header-footer-piano/v1/scripts/banner-header-footer-piano.js
+++ banner-header-footer-piano/v1/scripts/banner-header-footer-piano.js
@@ -19,7 +19,6 @@ for (i = 0; i < x.length; i++) {
 var tagSite = document.getElementById("banner-assinatura-header");
 
 if (tagSite) {
-	console.log('oi');
 	var aTag = document.createElement('a');
 	aTag.setAttribute('href',linkAssinaturaHeaderFooter);
 	aTag.setAttribute('style',"background-image: url("+imagemAssinaturaHeaderFooter+"); height: 44px;");
