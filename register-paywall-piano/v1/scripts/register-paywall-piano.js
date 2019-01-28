0ed121b31baa1c7d18b268228d931492313b7caa
vinnycl <carneiropresent@gmail.com>
2019-01-28 18:29:20 -0200

    Ajuste na URL de redirect e remocao de target blank

--- register-paywall-piano/v1/scripts/register-paywall-piano.js
+++ register-paywall-piano/v1/scripts/register-paywall-piano.js
@@ -4,7 +4,7 @@ let publicidadeFullBannerMobi = document.querySelector('#container-fullbanner');
 let paywallSiteContainer = document.querySelector('.paywall__site-container');
 let larguraTela = window.innerWidth;
 let alturaTela = window.innerHeight;
-let metadeTela = alturaTela/2;
+let metadeTela = alturaTela / 2;
 let uri = encodeURIComponent(document.location.href);
 let url = window.ambienteUtilizadoPiano == 'prd' ? 'https://login.globo.com/' : 'https://login.qa.globoi.com/';
 let protectedContentEl = document.querySelector(".protected-content");
@@ -12,27 +12,27 @@ let protectedContentEl = document.querySelector(".protected-content");
 // scroll top para manter o header
 document.documentElement.scrollTop = 0;
 
-if(paywallSiteContainer) {
-	
-	// remover conteudo da materia
-	if (protectedContentEl) {
-		protectedContentEl.setAttribute('data-content', 'removed');
-    	protectedContentEl.innerHTML = "Conteúdo exclusivo para assinantes.";
-	}
+if (paywallSiteContainer) {
 
-	if(headerBlog) {
-		paywallSiteContainer.insertAdjacentElement('afterBegin', headerBlog);
-	}
-	
-	if(barraGloboCom) {
-		paywallSiteContainer.insertAdjacentElement('afterBegin', barraGloboCom);
-	}
-	
-	if(publicidadeFullBannerMobi) {
-		paywallSiteContainer.insertAdjacentElement('afterend', publicidadeFullBannerMobi);
-	}
+    // remover conteudo da materia
+    if (protectedContentEl) {
+        protectedContentEl.setAttribute('data-content', 'removed');
+        protectedContentEl.innerHTML = "Conteúdo exclusivo para assinantes.";
+    }
 
-	let conteudoExperienciaRegisterPaywall = `
+    if (headerBlog) {
+        paywallSiteContainer.insertAdjacentElement('afterBegin', headerBlog);
+    }
+
+    if (barraGloboCom) {
+        paywallSiteContainer.insertAdjacentElement('afterBegin', barraGloboCom);
+    }
+
+    if (publicidadeFullBannerMobi) {
+        paywallSiteContainer.insertAdjacentElement('afterend', publicidadeFullBannerMobi);
+    }
+
+    let conteudoExperienciaRegisterPaywall = `
 		<div class="barreira-register-paywall" style="opacity: 0;">
 			<div class="barreira-register-paywall--content">
 				<div class="barreira-register-paywall--titulo">
@@ -45,7 +45,7 @@ if(paywallSiteContainer) {
 					</a>		
 				</div>
 				<div class="barreira-register-paywall--login">
-					Já possui cadastro? <a target="_blank" onclick="disparaEvento('Register / Paywall','Clique - Login','', this.href, event);" class="link" href="${url}login/${Piano.variaveis.getServicoId()}?tam=widget&amp;url=https%3A%2F%2Fintervencao.globo.com%2Fintervencoes%2Fshow.do%3Fpopin%3Dtrue%26servicoId%3D${Piano.variaveis.getServicoId()}%26urlIntervencao%3Dhttps%253A%252F%252Fs.glbimg.com%252Fgl%252Fba%252Fbarra-globocom.callback.html%2523${uri}" >Faça login</a> ou <a target="_blank" onclick="disparaEvento('Register / Paywall','Clique - Login','', this.href, event);" class="link" href="${url}cadastro/${Piano.variaveis.getServicoId()}?tam=widget&amp;url=https%3A%2F%2Fintervencao.globo.com%2Fintervencoes%2Fshow.do%3Fpopin%3Dtrue%26servicoId%3D${Piano.variaveis.getServicoId()}%26urlIntervencao%3Dhttps%253A%252F%252Fs.glbimg.com%252Fgl%252Fba%252Fbarra-globocom.callback.html%2523${uri}">Cadastre-se</a>.
+					Já possui cadastro? <a onclick="disparaEvento('Register / Paywall','Clique - Login','', this.href, event, true);" class="link" href="${url}login/${Piano.variaveis.getServicoId()}?url=https%3A%2F%2Fs3.glbimg.com%2Fv1%2FAUTH_65d1930a0bda476ba8d3c25c5371ec3f%2Fpiano%2Fhelper%2Fredirect.html%23${uri}" >Faça login</a> ou <a onclick="disparaEvento('Register / Paywall','Clique - Login','', this.href, event, true);" class="link" href="${url}cadastro/${Piano.variaveis.getServicoId()}?url=https%3A%2F%2Fs3.glbimg.com%2Fv1%2FAUTH_65d1930a0bda476ba8d3c25c5371ec3f%2Fpiano%2Fhelper%2Fredirect.html%23${uri}">Cadastre-se</a>.
 				</div>
 				<div class="barreira-register-paywall--oftsecundaria">
 					<a class="img2l" target="_blank" onclick="disparaEvento('Register / Paywall','Clique - Ver ofertas 2','', this.href, event);" href="#" >
@@ -60,87 +60,87 @@ if(paywallSiteContainer) {
 			</div>
 		</div>
 	`
-	paywallSiteContainer.insertAdjacentHTML('afterend', conteudoExperienciaRegisterPaywall);
+    paywallSiteContainer.insertAdjacentHTML('afterend', conteudoExperienciaRegisterPaywall);
 
-	let barreiraContainer = document.querySelector('.barreira-register-paywall');
+    let barreiraContainer = document.querySelector('.barreira-register-paywall');
 
-	//com a barreira criada, manipulando dom por mudanças externas
-	let barreiraOftprincipal = document.querySelector('.barreira-register-paywall--oftprincipal');
-	let barreiraOftsecundaria = document.querySelector('.barreira-register-paywall--oftsecundaria');
-	let barreiraTitulo = document.querySelector('.barreira-register-paywall--titulo');
-	let barreiraTextoLogin = document.querySelector('.barreira-register-paywall--login');
+    //com a barreira criada, manipulando dom por mudanças externas
+    let barreiraOftprincipal = document.querySelector('.barreira-register-paywall--oftprincipal');
+    let barreiraOftsecundaria = document.querySelector('.barreira-register-paywall--oftsecundaria');
+    let barreiraTitulo = document.querySelector('.barreira-register-paywall--titulo');
+    let barreiraTextoLogin = document.querySelector('.barreira-register-paywall--login');
 
-	// blocos editaveis
-	let img1m = document.querySelector('.img1m');
-	let img1d = document.querySelector('.img1d');
-	let img1l = document.querySelector('.img1l');
-	let img2m = document.querySelector('.img2m');
-	let img2d = document.querySelector('.img2d');
-	let img2l = document.querySelector('.img2l');
-	let img3m = document.querySelector('.img3m');
-	let img3d = document.querySelector('.img3d');
-	let img3l = document.querySelector('.img3l');
-
-
-	if (typeof barreiraOftprincipalContent != 'undefined' && barreiraOftprincipalContent != null && barreiraOftprincipalContent != '' ) {
-		barreiraOftprincipal.innerHTML = barreiraOftprincipalContent;
-	}
-
-	if (typeof barreiraOftsecundariaContent != 'undefined' && barreiraOftsecundariaContent != null && barreiraOftsecundariaContent != '' ) {
-		barreiraOftsecundaria.innerHTML = barreiraOftsecundariaContent;
-	}
-
-	if (typeof barreiraTituloContent != 'undefined' && barreiraTituloContent != null && barreiraTituloContent != '' ) {
-		barreiraTitulo.innerHTML = barreiraTituloContent;
-	}
-
-	if (typeof barreiraTextoLoginContent != 'undefined' && barreiraTextoLoginContent != null && barreiraTextoLoginContent != '' ) {
-		barreiraTextoLogin.innerHTML = barreiraTextoLoginContent;
-	}
-
-	if (typeof barreiraTextoLoginContentHide != 'undefined' && barreiraTextoLoginContentHide != null && barreiraTextoLoginContentHide != '' ) {
-		barreiraTextoLogin.innerHTML = " ";
-	}
-
-	if (typeof img1mL != 'undefined' && img1mL != null && img1mL != '' &&  img1dL != 'undefined' && img1dL != null && img1dL != '' && img1lL != 'undefined' && img1lL != null && img1lL != '') {
-		img1m.src = img1mL;
-		img1d.src = img1dL;
-		img1l.href = img1lL;
-	}
-
-	if (typeof img2mL != 'undefined' && img2mL != null && img2mL != '' &&  img2dL != 'undefined' && img2dL != null && img2dL != '' && img2lL != 'undefined' && img2lL != null && img2lL != '') {
-		img2m.src = img2mL;
-		img2d.src = img2dL;
-		img2l.href = img2lL;
-	}
-
-	if (typeof img3mL != 'undefined' && img3mL != null && img3mL != '' &&  img3dL != 'undefined' && img3dL != null && img3dL != '' && img3lL != 'undefined' && img3lL != null && img3lL != '') {
-		img3m.src = img3mL;
-		img3d.src = img3dL;
-		img3l.href = img3lL;
-	}
+    // blocos editaveis
+    let img1m = document.querySelector('.img1m');
+    let img1d = document.querySelector('.img1d');
+    let img1l = document.querySelector('.img1l');
+    let img2m = document.querySelector('.img2m');
+    let img2d = document.querySelector('.img2d');
+    let img2l = document.querySelector('.img2l');
+    let img3m = document.querySelector('.img3m');
+    let img3d = document.querySelector('.img3d');
+    let img3l = document.querySelector('.img3l');
 
 
-	if(larguraTela > 820 && alturaTela > 350) {
-		barreiraContainer.style.marginTop = metadeTela+'px';
-	}
+    if (typeof barreiraOftprincipalContent != 'undefined' && barreiraOftprincipalContent != null && barreiraOftprincipalContent != '') {
+        barreiraOftprincipal.innerHTML = barreiraOftprincipalContent;
+    }
 
-	setTimeout(function() {
-		barreiraContainer.style.opacity = 1;
-	}, 1000);
+    if (typeof barreiraOftsecundariaContent != 'undefined' && barreiraOftsecundariaContent != null && barreiraOftsecundariaContent != '') {
+        barreiraOftsecundaria.innerHTML = barreiraOftsecundariaContent;
+    }
 
-	// GA
+    if (typeof barreiraTituloContent != 'undefined' && barreiraTituloContent != null && barreiraTituloContent != '') {
+        barreiraTitulo.innerHTML = barreiraTituloContent;
+    }
 
-	window['dataLayer'] = window['dataLayer'] || [];
+    if (typeof barreiraTextoLoginContent != 'undefined' && barreiraTextoLoginContent != null && barreiraTextoLoginContent != '') {
+        barreiraTextoLogin.innerHTML = barreiraTextoLoginContent;
+    }
 
-	function disparaEvento(categoria, acao, rotulo, elemento, event) {
-		event.preventDefault(event); 
-		dataLayer.push({ 'event': 'EventoGA', 'eventoGACategoria': categoria, 'eventoGAAcao': acao, 'eventoGARotulo': rotulo, 'eventoGAValor': 0, 'eventoGAInteracao': false });
-		setTimeout(function() {
-			window.open(elemento, '_blank');
-		}, 300);
-	}
+    if (typeof barreiraTextoLoginContentHide != 'undefined' && barreiraTextoLoginContentHide != null && barreiraTextoLoginContentHide != '') {
+        barreiraTextoLogin.innerHTML = " ";
+    }
+
+    if (typeof img1mL != 'undefined' && img1mL != null && img1mL != '' && img1dL != 'undefined' && img1dL != null && img1dL != '' && img1lL != 'undefined' && img1lL != null && img1lL != '') {
+        img1m.src = img1mL;
+        img1d.src = img1dL;
+        img1l.href = img1lL;
+    }
+
+    if (typeof img2mL != 'undefined' && img2mL != null && img2mL != '' && img2dL != 'undefined' && img2dL != null && img2dL != '' && img2lL != 'undefined' && img2lL != null && img2lL != '') {
+        img2m.src = img2mL;
+        img2d.src = img2dL;
+        img2l.href = img2lL;
+    }
+
+    if (typeof img3mL != 'undefined' && img3mL != null && img3mL != '' && img3dL != 'undefined' && img3dL != null && img3dL != '' && img3lL != 'undefined' && img3lL != null && img3lL != '') {
+        img3m.src = img3mL;
+        img3d.src = img3dL;
+        img3l.href = img3lL;
+    }
+
+
+    if (larguraTela > 820 && alturaTela > 350) {
+        barreiraContainer.style.marginTop = metadeTela + 'px';
+    }
+
+    setTimeout(function() {
+        barreiraContainer.style.opacity = 1;
+    }, 1000);
+
+    // GA
+
+    window['dataLayer'] = window['dataLayer'] || [];
+
+    function disparaEvento(categoria, acao, rotulo, elemento, event, notBlank) {
+        event.preventDefault(event);
+        dataLayer.push({ 'event': 'EventoGA', 'eventoGACategoria': categoria, 'eventoGAAcao': acao, 'eventoGARotulo': rotulo, 'eventoGAValor': 0, 'eventoGAInteracao': false });
+        setTimeout(function() {
+            notBlank == true ? window.location.href = elemento : window.open(elemento);
+        }, 300);
+    }
 
 } else {
-	console.log("Tag de paywall nao existe");
+    console.log("Tag de paywall nao existe");
 }
