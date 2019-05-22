991d381460e67786af043ac528ba48fe1d89e436
lucas3ms <lucas3ms.ls@gmail.com>
2019-05-22 10:47:25 -0300

    Tornando o banner responsivo

--- swg/v1/script/anuncio-swg.js
+++ swg/v1/script/anuncio-swg.js
@@ -1,6 +1,6 @@
 const bannerSWG = (function() {
     
-    //let image = "https://d37iydjzbdkvr9.cloudfront.net/arquivos/2019/05/09/google-desk-2.png";
+    //let imagePiano = "https://d37iydjzbdkvr9.cloudfront.net/arquivos/2019/05/09/google-mobi-2.png";
     let imagePiano = "https://d37iydjzbdkvr9.cloudfront.net/arquivos/2019/05/09/google-desk-2.png"
     let linkPiano = "https://assinatura.oglobo.globo.com/VitrineDigital.aspx?id_parc=6236&cod_vitrine=112&campanha=sim"
     let textPiano = "<strong> O GLOBO</strong> em parceria com o <strong>Google</strong> oferece assinatura com <strong>três meses grátis</strong> e <strong>50% de desconto</strong> nos nove meses seguintes em apenas dois cliques</p>";
