44be722d23dc0b0bf1417fea07b61c471e63f3ae
lucas3ms <lucas3ms.ls@gmail.com>
2019-05-24 15:45:13 -0300

    Limpando o HTML

--- swg/v1/script/anuncio-swg.js
+++ swg/v1/script/anuncio-swg.js
@@ -1,6 +1,6 @@
 const bannerSWG = (function() {
     
-    const htmlAnuncio =`<div class="banner-swg" onclinck="">            
+    const htmlAnuncio =`<div class="banner-swg">            
                             <a class="banner-swg-link" href="${linkPiano}" target="_blank">    
                                 <div class="banner-swg-image" id="linkImage">
                                     <img class="banner-swg-image-img" alt="Assine com o Google" src="${imagePiano}"/>
