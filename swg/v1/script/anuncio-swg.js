19529490c3f5d469f6521e65ba91f2a09fb427e1
lucas3ms <lucas3ms.ls@gmail.com>
2019-05-24 15:06:40 -0300

    Ajustes finais no tamanho e posicionamento da imagem e do texto

--- swg/v1/script/anuncio-swg.js
+++ swg/v1/script/anuncio-swg.js
@@ -1,10 +1,5 @@
 const bannerSWG = (function() {
     
-    //let imagePiano = "https://d37iydjzbdkvr9.cloudfront.net/arquivos/2019/05/09/google-mobi-2.png";
-    let imagePiano = "https://d37iydjzbdkvr9.cloudfront.net/arquivos/2019/05/09/google-desk-2.png"
-    let linkPiano = "https://assinatura.oglobo.globo.com/VitrineDigital.aspx?id_parc=6236&cod_vitrine=112&campanha=sim"
-    let textPiano = "<strong> O GLOBO</strong> em parceria com o <strong>Google</strong> oferece assinatura com <strong>três meses grátis</strong> e <strong>50% de desconto</strong> nos nove meses seguintes em apenas dois cliques</p>";
-    
     const htmlAnuncio =`<div class="banner-swg" onclinck="">            
                             <a class="banner-swg-link" href="${linkPiano}" target="_blank">    
                                 <div class="banner-swg-image" id="linkImage">
@@ -18,7 +13,7 @@ const bannerSWG = (function() {
 
     function init(htmlAnuncio) {
         //var widgetHtmlSwgIn = document.querySelector('section.block.block--advertising.block--advertising-header');
-        const widgetHtmlSwgIn = document.querySelector('.teste');
+        const widgetHtmlSwgIn = document.querySelector('section.block.block--advertising.block--advertising-header');
 
         widgetHtmlSwgIn.insertAdjacentHTML("afterend", htmlAnuncio);
     }
