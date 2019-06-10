f6712ef5f5059ac83743e218a6c0343dad8c089f
riantavares <rian.tavares@live.com>
2019-06-10 20:06:29 -0300

    adding order

--- avatar-header-piano/v1/scripts/avatar-header-piano.js
+++ avatar-header-piano/v1/scripts/avatar-header-piano.js
@@ -3,11 +3,12 @@ var tagSite = document.getElementById("banner-assinatura-header");
 if (tagSite) {
 	var aTag = document.createElement('a');
     aTag.setAttribute('href',linkAvatarHeader);
+    aTag.setAttribute('style', "order: 2;");
     
     var divTag = document.createElement('div');
 	
 	if (window.matchMedia("(max-width: 767px)").matches) {
-		divTag.setAttribute('style',"background-image: url("+imagemAvatarHeader+"); width: 20px; height: 22px; order: 2; margin-left: 30px; background-size: 20px; background-repeat: no-repeat;");
+		divTag.setAttribute('style',"background-image: url("+imagemAvatarHeader+"); width: 20px; height: 22px; margin-left: 30px; background-size: 20px; background-repeat: no-repeat;");
 	}
     
     
