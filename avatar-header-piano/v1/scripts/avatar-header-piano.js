b92adbad680bdd57e3864866174c9cc5a8969fdc
riantavares <rian.tavares@live.com>
2019-06-10 20:49:41 -0300

    fixing avatar background position and margin

--- avatar-header-piano/v1/scripts/avatar-header-piano.js
+++ avatar-header-piano/v1/scripts/avatar-header-piano.js
@@ -8,7 +8,7 @@ if (tagSite) {
     var divTag = document.createElement('div');
 	
 	if (window.matchMedia("(max-width: 767px)").matches) {
-		divTag.setAttribute('style',"background-image: url("+imagemAvatarHeader+"); width: 20px; height: 22px; background-size: 20px; background-repeat: no-repeat;");
+		divTag.setAttribute('style',"background-image: url("+imagemAvatarHeader+"); height: 22px; background-size: 20px; background-repeat: no-repeat; background-position-x: right");
 	}
     
     
