cf59d293427e34b30e35f137960de3164d0434d6
riantavares <rian.tavares@live.com>
2019-06-10 20:30:09 -0300

    removing margin left of avatar

--- avatar-header-piano/v1/scripts/avatar-header-piano.js
+++ avatar-header-piano/v1/scripts/avatar-header-piano.js
@@ -3,12 +3,12 @@ var tagSite = document.getElementById("banner-assinatura-header");
 if (tagSite) {
 	var aTag = document.createElement('a');
     aTag.setAttribute('href',linkAvatarHeader);
-    aTag.setAttribute('style', "order: 2; width: 20px; height: 22px");
+    aTag.setAttribute('style', "order: 2; width: 55px; height: 22px");
     
     var divTag = document.createElement('div');
 	
 	if (window.matchMedia("(max-width: 767px)").matches) {
-		divTag.setAttribute('style',"background-image: url("+imagemAvatarHeader+"); width: 20px; height: 22px; margin-left: 30px; background-size: 20px; background-repeat: no-repeat;");
+		divTag.setAttribute('style',"background-image: url("+imagemAvatarHeader+"); width: 20px; height: 22px; background-size: 20px; background-repeat: no-repeat;");
 	}
     
     
