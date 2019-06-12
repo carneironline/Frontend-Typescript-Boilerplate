89c4a79001d159ca667bd2c04edc24ecc529f151
Gustavo Saboia <gustavosaboia97@hotmail.com>
2019-06-12 14:40:57 -0300

    Correção do tamanho do botao sair

--- avatar-header-piano/v1/scripts/avatar-header-piano.js
+++ avatar-header-piano/v1/scripts/avatar-header-piano.js
@@ -11,7 +11,7 @@
 
         aTag.setAttribute('href',linkAvatarHeader);
         aTag.setAttribute('style', 'order: 2; width: 20px; height: unset; margin-left: 20px;');
-		divTag.setAttribute('style','max-width: 100%; height: auto; display: block; ');
+		divTag.setAttribute('style','max-width: 100%; height: auto; display: block; height: 30px');
         divTag.setAttribute('src',imagemAvatarHeader);   
         divTag.setAttribute('id', 'btn-avatar-header');
     
