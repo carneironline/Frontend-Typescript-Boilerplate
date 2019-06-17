41e34c2c5ffa6201bfa4b15ad73dae6c67206989
riantavares <rian.tavares@live.com>
2019-06-17 16:47:20 -0300

    removing link, removing tag a and setting as div

--- avatar-header-piano/v1/scripts/avatar-header-piano.js
+++ avatar-header-piano/v1/scripts/avatar-header-piano.js
@@ -6,14 +6,13 @@
             return false;
 
     
-        const aTag = document.createElement('a');
+        const divTag = document.createElement('div');
         const imgTag = document.createElement('img');
         const searchWrapper = document.querySelector('.site-header__search-wrapper');
         const siteHeader = document.querySelector('.site-header__banner-assinatura-wrapper');
 
 
-        aTag.setAttribute('href',linkAvatarHeader);
-        aTag.setAttribute('style', 'order: 2; width: 18px; height: unset; margin-left: 18px;');
+        divTag.setAttribute('style', 'order: 2; width: 18px; height: unset; margin-left: 18px;');
 		imgTag.setAttribute('style','max-width: 100%; height: auto; display: block;');
         imgTag.setAttribute('src',imagemAvatarHeader);   
         imgTag.setAttribute('id', 'btn-avatar-header');
@@ -24,8 +23,8 @@
         searchWrapper.style.marginRight = 0;
         siteHeader.style.marginRight = 0;
         
-        aTag.appendChild(imgTag);
-        tagSite.appendChild(aTag);
+        divTag.appendChild(imgTag);
+        tagSite.appendChild(divTag);
     }
 
 })();
