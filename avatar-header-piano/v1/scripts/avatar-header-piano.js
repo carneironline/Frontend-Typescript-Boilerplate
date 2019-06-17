d04c94942f71b56747beeeb0d3c6addd471b2be1
riantavares <rian.tavares@live.com>
2019-06-17 18:38:42 -0300

    adding tooltip div, setting some attribs off and setting hover to avatar to show tooltip

--- avatar-header-piano/v1/scripts/avatar-header-piano.js
+++ avatar-header-piano/v1/scripts/avatar-header-piano.js
@@ -7,24 +7,32 @@
 
     
         const divTag = document.createElement('div');
+        const toolTip = document.createElement('div');
         const imgTag = document.createElement('img');
         const searchWrapper = document.querySelector('.site-header__search-wrapper');
         const siteHeader = document.querySelector('.site-header__banner-assinatura-wrapper');
+        const btnAssine = document.querySelector('#btn-assine-header');       
 
-
+        divTag.setAttribute('class', 'account-avatar');
         divTag.setAttribute('style', 'order: 2; width: 18px; height: unset; margin-left: 18px;');
 		imgTag.setAttribute('style','max-width: 100%; height: auto; display: block;');
         imgTag.setAttribute('src',imagemAvatarHeader);   
         imgTag.setAttribute('id', 'btn-avatar-header');
-    
+        toolTip.setAttribute('class', 'account-tooltip');
+        toolTip.setAttribute('style', 'position: absolute; width: 128px; height: 116px; top: 30px; right: 0; border-radius: 5px; box-shadow: 0 0 2px 0 rgba(0, 0, 0, 0.36); border: solid 1px #cccccc; background-color: #ffffff; z-index: 9;');
+        
+
         tagSite.style.display = 'flex';
         tagSite.style.flexDirection = 'row';
         tagSite.style.alignItems = 'center';
         searchWrapper.style.marginRight = 0;
         siteHeader.style.marginRight = 0;
+        btnAssine.style.overflow = 'unset';
+        siteHeader.style.overflow = 'unset';
         
         divTag.appendChild(imgTag);
         tagSite.appendChild(divTag);
+        tagSite.appendChild(toolTip);
     }
 
 })();
