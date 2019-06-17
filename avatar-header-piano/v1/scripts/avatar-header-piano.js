3f3997bc36f84a34af2c18f9da9520c928988a79
riantavares <rian.tavares@live.com>
2019-06-17 20:00:23 -0300

    adding info with style into tooltip

--- avatar-header-piano/v1/scripts/avatar-header-piano.js
+++ avatar-header-piano/v1/scripts/avatar-header-piano.js
@@ -9,6 +9,10 @@
         const divTag = document.createElement('div');
         const toolTip = document.createElement('div');
         const imgTag = document.createElement('img');
+        const buttonTag = document.createElement('button');
+        const pTag = document.createElement('p');
+        const uTag = document.createElement('u');
+        const liTag = document.createElement('li');
         const searchWrapper = document.querySelector('.site-header__search-wrapper');
         const siteHeader = document.querySelector('.site-header__banner-assinatura-wrapper');
         const btnAssine = document.querySelector('#btn-assine-header');       
@@ -20,7 +24,11 @@
         imgTag.setAttribute('id', 'btn-avatar-header');
         toolTip.setAttribute('class', 'account-tooltip');
         toolTip.setAttribute('style', 'position: absolute; display: none; width: 128px; height: 116px; top: 30px; right: 0; border-radius: 5px; box-shadow: 0 0 2px 0 rgba(0, 0, 0, 0.36); border: solid 1px #cccccc; background-color: #ffffff; z-index: 9;');
-        
+        pTag.innerHTML = 'Olá';
+        pTag.setAttribute('style', 'font-family: OpenSans; font-size: 13px; color: #7a7a7a; margin-left: 14px;');
+        uTag.setAttribute('style', 'margin-inline-start: -1em; font-family: OpenSans; color: #7a7a7a');
+        liTag.innerHTML = 'minha conta';
+        buttonTag.setAttribute('style', 'width: 39px; height: 19px; border-radius: 2px; border-style: solid; border-width: 1px; border-image-source: linear-gradient(to bottom, #e5e6e6, #b8b8b8); border-image-slice: 1; background-image: linear-gradient(to bottom, #ffffff, #efefef), linear-gradient(to bottom, #e5e6e6, #b8b8b8); background-origin: border-box; margin-left: 78px; font-family: OpenSans; font-size: 10px; font-weight: 600; color: #9a9a9a;');
 
         tagSite.style.display = 'flex';
         tagSite.style.flexDirection = 'row';
@@ -29,10 +37,18 @@
         siteHeader.style.marginRight = 0;
         btnAssine.style.overflow = 'unset';
         siteHeader.style.overflow = 'unset';
+
+        toolTip.appendChild(pTag);
+        uTag.appendChild(liTag);
+        toolTip.appendChild(uTag);
+        toolTip.appendChild(buttonTag);
+
         
         divTag.appendChild(imgTag);
         divTag.appendChild(toolTip);
         tagSite.appendChild(divTag);
+
+        
     }
 
 })();
