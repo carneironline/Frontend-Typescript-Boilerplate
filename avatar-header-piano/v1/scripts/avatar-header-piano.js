3667c543b1497043178b4047232d21e1c55b14a0
riantavares <rian.tavares@live.com>
2019-06-19 10:24:20 -0300

    trying to insert font and removing tag a inside the button and setting button as tag a

--- avatar-header-piano/v1/scripts/avatar-header-piano.js
+++ avatar-header-piano/v1/scripts/avatar-header-piano.js
@@ -19,9 +19,9 @@
         const siteHeader = document.querySelector('.site-header__banner-assinatura-wrapper');
         const btnAssine = document.querySelector('#btn-assine-header');       
 
-        logoutLink.setAttribute('href', linkAccountLogout);
+        buttonTag.setAttribute('href', linkAccountLogout);
         myAccount.setAttribute('href', linkMyAccount);
-        myAccount.setAttribute('style', 'margin-inline-start: -1em; font-family: OpenSans; color: #7a7a7a');
+        myAccount.setAttribute('style', 'margin-inline-start: -1em; font-family: Open-Sans-Regular; color: #7a7a7a');
         divTag.setAttribute('class', 'account-avatar');
         divTag.setAttribute('style', 'order: 2; width: 18px; height: unset; margin-left: 18px;');
 		imgTag.setAttribute('style','max-width: 100%; height: auto; display: block;');
@@ -30,11 +30,11 @@
         toolTip.setAttribute('class', 'account-tooltip');
         toolTip.setAttribute('style', 'position: absolute; display: none; width: 128px; height: 116px; top: 30px; right: -4px; border-radius: 5px; box-shadow: 0 0 2px 0 rgba(0, 0, 0, 0.36); border: solid 1px #cccccc; background-color: #ffffff; z-index: 9;');
         pTag.innerHTML = 'Olá';
-        pTag.setAttribute('style', 'font-family: OpenSans; font-size: 13px; color: #7a7a7a; margin-left: 14px;');
-        uTag.setAttribute('style', 'margin-inline-start: -1em; font-family: OpenSans; color: #7a7a7a');
+        pTag.setAttribute('style', 'font-family: Open-Sans-SemiBold; font-size: 13px; color: #7a7a7a; margin-left: 14px;');
+        uTag.setAttribute('style', 'margin-inline-start: -1em; font-family: Open-Sans-Regular; color: #7a7a7a');
         liTag.innerHTML = 'minha conta';
         buttonTag.innerHTML = 'SAIR';
-        buttonTag.setAttribute('style', 'width: 39px; height: 19px; border-radius: 2px; border-style: solid; border-width: 1px; border-image-source: linear-gradient(to bottom, #e5e6e6, #b8b8b8); border-image-slice: 1; background-image: linear-gradient(to bottom, #ffffff, #efefef), linear-gradient(to bottom, #e5e6e6, #b8b8b8); background-origin: border-box; margin-left: 78px; font-family: OpenSans; font-size: 10px; font-weight: 600; color: #9a9a9a;');
+        buttonTag.setAttribute('style', 'width: 39px; height: 19px; border-radius: 2px; border-style: solid; border-width: 1px; border-image-source: linear-gradient(to bottom, #e5e6e6, #b8b8b8); border-image-slice: 1; background-image: linear-gradient(to bottom, #ffffff, #efefef), linear-gradient(to bottom, #e5e6e6, #b8b8b8); background-origin: border-box; margin-left: 78px; font-family: Open-Sans-SemiBold; font-size: 10px; font-weight: 600; color: #9a9a9a;');
 
         tagSite.style.display = 'flex';
         tagSite.style.flexDirection = 'row';
