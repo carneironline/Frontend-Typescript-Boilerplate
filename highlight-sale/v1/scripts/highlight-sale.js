91440515ca099c7656ef3772b29cf0dd1bc17765
Marcio Sardou <marciosut1996@gmail.com>
2019-07-05 17:42:26 -0300

    Adding fonts and font-face, setting elements contents and fixing style

--- highlight-sale/v1/scripts/highlight-sale.js
+++ highlight-sale/v1/scripts/highlight-sale.js
@@ -6,16 +6,24 @@
         const divTitle = document.createElement('div');
         const divText = document.createElement('div');
         const buttonImgTag = document.createElement('img');
+        const buttonImgUrl = document.createElement('a');
         
 
         section.setAttribute('id', 'highlight-sale-section');
         divTitle.setAttribute('id','highlight-sale-title' );
         divText.setAttribute('class', 'highlight-sale-text');
+        buttonImgUrl.setAttribute('class', 'highlight-sale-button-url')
         buttonImgTag.setAttribute('class', 'highlight-sale-button');
+
+        highlightSaleTitle ? divTitle.innerHTML = highlightSaleTitle : '';
+        highlightSaleText ? divText.innerHTML = highlightSaleText : '';
+        highlightSaleButtonImg ? buttonImgTag.setAttribute('src', highlightSaleButtonImg) : '';
+        highlightSaleButtonImgUrl ? buttonImgUrl.setAttribute('href', highlightSaleButtonImgUrl) : '';
 
         section.appendChild(divTitle);
         section.appendChild(divText);
-        section.appendChild(buttonImgTag);
+        buttonImgUrl.appendChild(buttonImgTag);
+        section.appendChild(buttonImgUrl);
         highlightSale.innerHTML = section;
 
 
