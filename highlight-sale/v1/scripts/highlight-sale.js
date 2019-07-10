a04292b266334902a7611d3bc0c89035138e75f1
riantavares <rian.tavares@live.com>
2019-07-10 14:18:46 -0300

    treating variables to return if they are undefined

--- highlight-sale/v1/scripts/highlight-sale.js
+++ highlight-sale/v1/scripts/highlight-sale.js
@@ -1,33 +1,50 @@
 (function (){
-    if (document.getElementById('highlight-sale')) {
+    if (!document.getElementById('highlight-sale'))
+        return;
 
-        const highlightSale = document.getElementById('highlight-sale');
-        const section = document.createElement('section');
-        const divTitle = document.createElement('div');
-        const divText = document.createElement('div');
-        const buttonImgTag = document.createElement('img');
-        const buttonImgUrl = document.createElement('a');
-        
+    const highlightSale = document.getElementById('highlight-sale');
+    const section = document.createElement('section');
+    const divTitle = document.createElement('div');
+    const divText = document.createElement('div');
+    const buttonImgTag = document.createElement('img');
+    const buttonImgUrl = document.createElement('a');
+    
 
-        section.setAttribute('id', 'highlight-sale-section');
-        divTitle.setAttribute('class','highlight-sale-title' );
-        divText.setAttribute('class', 'highlight-sale-text');
-        buttonImgUrl.setAttribute('class', 'highlight-sale-button-url')
-        buttonImgTag.setAttribute('class', 'highlight-sale-button');
+    section.setAttribute('id', 'highlight-sale-section');
+    divTitle.setAttribute('class','highlight-sale-title' );
+    divText.setAttribute('class', 'highlight-sale-text');
+    buttonImgUrl.setAttribute('class', 'highlight-sale-button-url')
+    buttonImgTag.setAttribute('class', 'highlight-sale-button');
 
+    if (typeof highlightSaleTitle !== 'undefined') {
         highlightSaleTitle ? divTitle.innerHTML = highlightSaleTitle : '';
-        highlightSaleText ? divText.innerHTML = highlightSaleText : '';
-        highlightSaleButtonImg ? buttonImgTag.setAttribute('src', highlightSaleButtonImg) : '';
-        highlightSaleButtonImgUrl ? buttonImgUrl.setAttribute('href', highlightSaleButtonImgUrl) : '';
-
-        section.appendChild(divTitle);
-        section.appendChild(divText);
-        buttonImgUrl.appendChild(buttonImgTag);
-        section.appendChild(buttonImgUrl);
-        highlightSale.appendChild(section);
-
-
+    } else {
+        return;
     }
+
+    if (typeof highlightSaleText !== 'undefined') {
+        highlightSaleText ? divText.innerHTML = highlightSaleText : '';
+    } else {
+        return;
+    }
+
+    if (typeof highlightSaleButtonImg !== 'undefined') {
+        highlightSaleButtonImg ? buttonImgTag.setAttribute('src', highlightSaleButtonImg) : '';
+    } else {
+        return;
+    }
+
+    if (typeof highlightSaleButtonImgUrl !== 'undefined') {
+        highlightSaleButtonImgUrl ? buttonImgUrl.setAttribute('href', highlightSaleButtonImgUrl) : '';
+    } else {
+        return;
+    }
+
+    section.appendChild(divTitle);
+    section.appendChild(divText);
+    buttonImgUrl.appendChild(buttonImgTag);
+    section.appendChild(buttonImgUrl);
+    highlightSale.appendChild(section);
 })();
 
 
