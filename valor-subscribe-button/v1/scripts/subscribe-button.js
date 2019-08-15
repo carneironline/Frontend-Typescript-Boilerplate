2d09300d7fec3389769a5eb4f96708c9dcccca6b
riantavares <rian.tavares@live.com>
2019-08-15 18:44:12 -0300

    fixing validation of empty var

--- valor-subscribe-button/v1/scripts/subscribe-button.js
+++ valor-subscribe-button/v1/scripts/subscribe-button.js
@@ -1,8 +1,8 @@
 (function (){
 
     const subscribeDiv = document.querySelector('.header__actions__action.header__subscribe');
-    const imageValue = (typeof buttonValorImg !== 'undefined' && !buttonValorImg.trim()) ? true : false;
-    const urlValue = (typeof buttonValorUrl !== 'undefined' && !buttonValorUrl.trim()) ? true : false;
+    const imageValue = (typeof buttonValorImg !== 'undefined' && Boolean(buttonValorImg.trim())) ? true : false;
+    const urlValue = (typeof buttonValorUrl !== 'undefined' && Boolean(buttonValorUrl.trim())) ? true : false;
     const button = document.createElement('a');
     const buttonImg = document.createElement('img');
 
