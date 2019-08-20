5af8301c21a3a00af2f97c73d968182a58351db5
Marcio Sardou <marciosut1996@gmail.com>
2019-08-20 12:03:39 -0300

    solving develop conficts and adding crescer

--- js/tiny.js
+++ js/tiny.js
@@ -108,6 +108,7 @@ Piano.variaveis = {
 			case 'globo-rural':
 			case 'gq':
 			case 'monet':
+			case 'crescer':
 				return nomeProduto;
 			case 'casa-e-jardim':
 				return 'casa-jardim';
@@ -799,7 +800,7 @@ Piano.util = {
 		document.body.insertBefore(e, document.body.lastChild);
 	},
 	isRevista: function(){
-		var revistas = ["epoca", "auto-esporte", "vogue", "glamour", "casa-vogue", "marie-claire", "casa-e-jardim", "quem-acontece", "globo-rural", "gq", "monet"];
+		var revistas = ["epoca", "auto-esporte", "vogue", "glamour", "casa-vogue", "marie-claire", "casa-e-jardim", "quem-acontece", "globo-rural", "gq", "monet", 'crescer'];
 		if(revistas.indexOf(Piano.variaveis.getNomeProduto()) > -1)
 			return true;
 		else
