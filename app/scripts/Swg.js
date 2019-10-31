3d4e39e62907afbe9fd48f6d5491a0610dd82044
Rodrigo Vinicius Alves Carneiro - Estratégia Digital - Infoglobo <rcarneiro@OGMASTER.LOCAL>
2019-10-31 13:54:21 -0300

    passando o swgProductId dinamicamente pelo glbPaywall

--- app/scripts/Swg.js
+++ app/scripts/Swg.js
@@ -27,9 +27,17 @@ export default class Swg {
         };
     }
 
-    setUtms() {                 
+    setUtms(element) {                 
         const urlParams = new URLSearchParams(window.location.search.substring(1));
         const utmsProps = (typeof window.glbPaywall.swg !== 'undefined' && typeof window.glbPaywall.swg.utms !== 'undefined') ? window.glbPaywall.swg.utms : null; 
+        let swgProductId = null;
+
+        switch(element.dataset.area) {
+            case 'top': swgProductId = window.glbPaywall.topSwgProductid; break;
+            case 'left': swgProductId = window.glbPaywall.leftSwgProductid; break;
+            case 'right': swgProductId = window.glbPaywall.rightSwgProductid; break;
+        }
+
 
         utmsProps.forEach((item) => { 
             let name = item.name.toLowerCase();
@@ -45,7 +53,8 @@ export default class Swg {
 
         if( (this.disabled || !this.isDefined) || !utmsProps ) return;
         
-        window.tinyCpt.Swg.global.subscribe('br.com.infoglobo.oglobo.swg.google');
+        if(swgProductId)
+            window.tinyCpt.Swg.global.subscribe(swgProductId);
     }
 
     async getProducts() {
