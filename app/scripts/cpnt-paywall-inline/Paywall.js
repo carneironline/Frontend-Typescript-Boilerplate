d32d38bb1373c8b3453d21ce1c492e6b3a474484
Rodrigo Carneiro <carneironline@gmail.com>
2020-02-19 18:48:57 -0300

    ajuste de barreira do analitico

--- app/scripts/cpnt-paywall-inline/Paywall.js
+++ app/scripts/cpnt-paywall-inline/Paywall.js
@@ -34,21 +34,32 @@ export default class PaywallCptInline  {
 	}
 
     activeEvents() {
-		const targetPost = window.analiticoPost;
+		const postElement = window.analiticoPost;
 
-		if(!targetPost) return null
+		if(!postElement) return null
 
-		const postBox = targetPost.querySelector(".btn-read-more");
-
-		this.createTemplate(postBox)
+		this.createTemplate(postElement)
 	}
 
-  	createTemplate(element) {
+  	createTemplate(postElement) {
+		const element = postElement.querySelector(".btn-read-more");
+		const mainElement = postElement.querySelector('.main-content')
+		const opacityElement = `<div class="paywall-cpt-inline-opacity"></div>`
+
 		if (element) {
 			if(!window.glbPaywallInline.cssLoaded) {
 				document.body.insertAdjacentHTML('beforebegin', this.cssMinified); 
 				window.glbPaywallInline.cssLoaded = true
 			}
+
+			mainElement.setAttribute('style', `
+			background: linear-gradient(180deg,hsla(0,0%,100%,0) 0,#fff 40%,#fff 100%);
+			position: relative;
+			height: 160px;
+			overflow: hidden;`);
+
+			mainElement.insertAdjacentHTML('beforeend', opacityElement); 
+			
 			
 			element.insertAdjacentHTML('beforebegin', this.template); 
 			const removedElement = Array.from(element.parentNode.parentNode.childNodes).find((element) => element.className === 'other-content')
@@ -119,7 +130,7 @@ export default class PaywallCptInline  {
   get cssMinified() {
 	  return `
 	  <style>
-	  .paywall-cpt-inline{margin: 0 auto 40px;padding:20px;border-radius: 4px;border: solid 1px #dbdde1;background-color: #f2f2f5;}.paywall-cpt-inline-title{margin-left:auto;margin-right:auto; margin-bottom:30px;display:block;font-family:Raleway;font-size:20px;font-weight:700;font-stretch:normal;font-style:normal;line-height:1.2;letter-spacing:normal;text-align:center;color:#325e94}
+	  .paywall-cpt-inline{margin: 0 auto 40px;padding:20px;border-radius: 4px;border: solid 1px #dbdde1;background-color: #f2f2f5;}.paywall-cpt-inline-opacity{background: linear-gradient(180deg,hsla(0,0%,100%,0) 0,#fff 40%,#fff 100%);position: relative;height: 160px;overflow: hidden;}.paywall-cpt-inline-title{margin-left:auto;margin-right:auto; margin-bottom:30px;display:block;font-family:Raleway;font-size:20px;font-weight:700;font-stretch:normal;font-style:normal;line-height:1.2;letter-spacing:normal;text-align:center;color:#325e94}
 	  .paywall-cpt-inline-button{margin-left:auto;margin-right:auto;margin-bottom:30px;display:block;width:256px;height:48px;background-color:#f59c00;border-radius:4px;border:solid 3px #f59c00;}
 	  @media screen and (min-width: 424px) {
 		.paywall-cpt-inline-button { width: 356px; }
