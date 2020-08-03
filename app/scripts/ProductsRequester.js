297ec82e235206ab50f320726cfa2374889efeed
lucas3ms <lucas3ms.ls@gmail.com>
2020-08-03 16:49:54 -0300

    Ajuste nas urls do vault para obter o products.json

--- app/scripts/ProductsRequester.js
+++ app/scripts/ProductsRequester.js
@@ -1,14 +1,11 @@
 export default function getProductsObject(environment, callback) {
-    var httpMethod = 'GET';
-    var prodJson = 'https://s3.glbimg.com/v1/AUTH_3426e47ed6784b729ddc152696060e4c/tiny/products.json';
-    var qaJson = 'https://s3.glbimg.com/v1/AUTH_a00f952f76904b5ca11e8426a3b0c8f8/tiny/products.json';
-    var finalURL = qaJson;
-    var asyncRequest = false;
+    const httpMethod = 'GET';
+    const prodJson = 'https://s3.glbimg.com/v1/AUTH_7b0a6df49895459fbafe49a96fcb5bbf/tiny/Prod/produtos.json';
+    const qaJson = 'https://s3.glbimg.com/v1/AUTH_7b0a6df49895459fbafe49a96fcb5bbf/tiny/Hom/produtos.json';
+    const finalURL = window.tinyCpt.isProduction ? prodJson : qaJson;
+    const asyncRequest = false;
 
-    if (environment === 'prd')
-        finalURL = prodJson;
-
-    var requester = new XMLHttpRequest();
+    let requester = new XMLHttpRequest();
 
     requester.onreadystatechange = function(){
         if (requester.readyState === XMLHttpRequest.DONE){
