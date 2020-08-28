14e38432506ad84668797e4aabcc50901e90c8c4
riantavares <rian.tavares@live.com>
2020-08-28 15:38:20 -0300

    alterando o nome da barreira e ajustando as variaveis para formato de objeto

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -540,14 +540,14 @@ window.Piano.paywall = {
             `https://static${window.Piano.util.montaUrlStg()}.infoglobo.com.br/paywall/barreira/nao-barreira-gcom/scripts/index.js`
         )
     },
-    barreiraMobile() {
+    barreiraBarbeira() {
         window.Piano.util.adicionarCss(
-            `<link rel='stylesheet' type='text/css' href='https://static${window.Piano.util.montaUrlStg()}.infoglobo.com.br/paywall/barreira/barreira-mobi/styles/styles.css'>`
+            `<link rel='stylesheet' type='text/css' href='https://static${window.Piano.util.montaUrlStg()}.infoglobo.com.br/paywall/barreira/barreira-barbeira/styles/styles.css'>`
         )
         window.Piano.xmlHttpRequest.geraScriptNaPagina(
-            `https://static${window.Piano.util.montaUrlStg()}.infoglobo.com.br/paywall/barreira/barreira-mobi/scripts/index.js`
-        ) 
-    }
+            `https://static${window.Piano.util.montaUrlStg()}.infoglobo.com.br/paywall/barreira/barreira-barbeira/scripts/index.js`
+        )
+    },
 }
 
 function analyticalUnblockedForPiano() {
