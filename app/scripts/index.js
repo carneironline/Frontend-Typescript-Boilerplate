a0660b11d66c2bc8b713d5ccd3792b3c543e0d64
riantavares <rian.tavares@live.com>
2020-10-01 14:22:37 -0300

    removendo chamada da barreira barbeira e ajustando setGa em comunicado v1

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -567,14 +567,6 @@ window.Piano.paywall = {
             `https://static${window.Piano.util.montaUrlStg()}.infoglobo.com.br/paywall/barreira/nao-barreira/scripts/index.js`
         )
     },
-    barreiraBarbeira() {
-        window.Piano.util.adicionarCss(
-            `<link rel='stylesheet' type='text/css' href='https://static${window.Piano.util.montaUrlStg()}.infoglobo.com.br/paywall/barreira/barreira-barbeira/styles/styles.css'>`
-        )
-        window.Piano.xmlHttpRequest.geraScriptNaPagina(
-            `https://static${window.Piano.util.montaUrlStg()}.infoglobo.com.br/paywall/barreira/barreira-barbeira/scripts/index.js`
-        )
-    },
 }
 
 function analyticalUnblockedForPiano() {
