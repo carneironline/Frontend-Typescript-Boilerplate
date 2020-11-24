9e8abad351354b79829b774a055ba77986f154d0
Rodrigo Carneiro <carneironline@gmail.com>
2020-11-24 17:37:03 -0300

    ajuste de bannerCover

--- app/components/BannerCover/index.js
+++ app/components/BannerCover/index.js
@@ -1,29 +1,29 @@
 class BannerCover {
 
-	constructor() {		 
-		this.classMain = 'banner-cover-cpnt'
+    constructor() {
+        this.classMain = 'banner-cover-cpnt'
         this.styleClassMain = `.${this.classMain}`
-		this.elBody = document.body
+        this.elBody = document.body
         this.elCpnt = null
-		this.elTarget = document.querySelector('.banner-cover-cpnt-aim, .block--advertising-header')
-		
-		if(!this.elTarget) return null
+        this.elTarget = document.querySelector('.banner-cover-cpnt, .block--advertising-header')
 
-		this.init()
-		this.showConsole()
-	}
+        if (!this.elTarget) return null
 
-	init() {
-		window.glbBannerCover = window.glbBannerCover ? { ...this.settings(), ...window.glbBannerCover } : this.settings()
+        this.init()
+        this.showConsole()
+    }
 
-		this.addStyle()
-		this.add()
-		this.show()
-		this.click()
-		this.setGa()
-	}
+    init() {
+        window.glbBannerCover = window.glbBannerCover ? { ...this.settings(), ...window.glbBannerCover } : this.settings()
 
-	showConsole() {
+        this.addStyle()
+        this.add()
+        this.show()
+        this.click()
+        this.setGa()
+    }
+
+    showConsole() {
         console.groupCollapsed('BannerCover Component')
 
         console.group('Constructor')
@@ -35,100 +35,101 @@ class BannerCover {
         console.groupEnd()
 
         console.groupEnd()
-	}
+    }
 
-	setGa() {
-		const calledBy = 'BannerCover'
-		const action = window.glbBannerCover?.ga?.action || 'Mobiliario Chamada de Capa';
-		const label = window.glbBannerCover?.ga?.label;
-		
-		if(label) {
-			window.tinyCpnt.GA.setEvents( 
-				calledBy,
-				action,
-				label,
-			)
-		}
-	}
-	
-	settings() {
-		return {
-			image: 'https://via.placeholder.com/296x157',
-			url: '//google.com',
-			text: 'Test', // Test
-		}
-	}
+    setGa() {
+        const calledBy = 'BannerCover'
+        const action = window.glbBannerCover?.ga?.action || 'Mobiliario Chamada de Capa';
+        const label = window.glbBannerCover?.ga?.label;
 
-	add() { 
-		this.elTarget.insertAdjacentHTML('afterend', this.render())
-		this.elCpnt = document.querySelector(`${this.styleClassMain}`)
-	}
+        if (label) {
+            window.tinyCpnt.GA.setEvents(
+                calledBy,
+                action,
+                label,
+            )
+        }
+    }
 
-	show() {
-		setTimeout(() => {
-			this.elCpnt.classList.remove(`${this.classMain}--hide`)
-		}, 500)
-	}
-	
-	addStyle() {
+    settings() {
+        return {
+            image: 'https://via.placeholder.com/296x157',
+            url: '//google.com',
+            text: 'Test', // Test
+        }
+    }
+
+    add() {
+        const insertPosition = window.tinyCpnt.Product.name === 'oglobo' ? 'afterend' : 'beforeend'
+        this.elTarget.insertAdjacentHTML(insertPosition, this.render())
+        this.elCpnt = document.querySelector(`${this.styleClassMain}-box`)
+    }
+
+    show() {
+        setTimeout(() => {
+            this.elCpnt.classList.remove(`${this.classMain}--hide`)
+        }, 500)
+    }
+
+    addStyle() {
         this.elBody.insertAdjacentHTML('beforeend', this.style())
-	}
-	
-	click() {
-		const element = document.querySelector(`${this.styleClassMain}-target`)	
-		if(!element) return null
+    }
 
-		element.addEventListener('click', (evt) => {
-			evt.preventDefault()
-			
-			setTimeout(() => {
-				window.open(element.href)
-			}, 500)
-		})
-	}
+    click() {
+        const element = document.querySelector(`${this.styleClassMain}-target`)
+        if (!element) return null
 
-	render() { 
+        element.addEventListener('click', (evt) => {
+            evt.preventDefault()
+
+            setTimeout(() => {
+                window.open(element.href)
+            }, 500)
+        })
+    }
+
+    render() {
         return `
-        <div class="${this.classMain} ${this.classMain}--hide">            
-            <a class="${this.classMain}-target" href="${window.glbBannerCover.url}" target="_blank">    
+        <div class="${this.classMain}-box ${this.classMain}--hide">
+            <a class="${this.classMain}-target" href="${window.glbBannerCover.url}" target="_blank">
                 <div class="${this.classMain}-image" id="linkImage">
                     <img class="${this.classMain}-image-img" alt="" src="${window.glbBannerCover.image}"/>
                 </div>
                 <div class="${this.classMain}-text">
                     <div class="main-title">${window.glbBannerCover.text}</div>
                 </div>
-            </a>       
+            </a>
         </div>
 		`
-	}
+    }
 
-	style() {
-		return `
+    style() {
+        return `
 		<style>
-			 ${this.styleClassMain} {
-				cursor: pointer; 
+			 ${this.styleClassMain}-box {
+				cursor: pointer;
 				opacity: 1;
                 transition: opacity .4s;
-				
+
 				margin: 0 auto 0px;
 				max-width: 1260px;
 				padding: 0px 0px 40px;
 			}
-			
+
 			@media (max-width: 1023px) {
-				${this.styleClassMain} {
+				${this.styleClassMain}-box {
 					padding: 40px 0px 40px;
 				}
 			}
 
 			${this.styleClassMain}--hide {
-				opacity: 0; 
+				opacity: 0;
 			}
-			
-			.main-content ${this.styleClassMain} {
+
+			.main-content ${this.styleClassMain}-box {
 				padding: 80px 0 0px;
 			}
-			
+
 			${this.styleClassMain}-target {
 				text-decoration: none;
 				color: currentColor;
@@ -139,7 +140,7 @@ class BannerCover {
 				border-top: #ddd 1px solid;
 				border-bottom: #ddd 1px solid;
 			}
-			
+
 			${this.styleClassMain}-image {
 				position: relative;
 				width: calc(21%);
@@ -149,9 +150,9 @@ class BannerCover {
 				${this.styleClassMain}-image {
 					width: unset;
 				}
-			
+
 			}
-			
+
 			${this.styleClassMain}-image-img {
 				position: absolute;
 				right: 0;
@@ -164,14 +165,14 @@ class BannerCover {
 					max-width: 87%;
 				}
 			}
-			
+
 			${this.styleClassMain}-text {
 				font-family: 'Source Sans Pro', sans-serif;
 				width: 60%;
 				padding: 15px 10% 15px 20px;
 				color: darkslategray;
 			}
-			
+
 			@media (max-width: 1023px) {
 				${this.styleClassMain}-text {
 					font-size: 86%;
@@ -179,7 +180,7 @@ class BannerCover {
 			}
 		</style>
 		`
-	}
+    }
 }
 
 export default BannerCover
