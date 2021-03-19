b3756e3beb916318573db469199b4659368febbb
Rodrigo Carneiro <carneironline@gmail.com>
2021-03-19 10:25:26 -0300

    removendo componentes desnecessários

--- app/index.ts
+++ app/index.ts
@@ -1,20 +1,6 @@
-import AddQueryOnLinks from '@components/AddQueryOnLinks'
-import ChatCpnt from '@components/ChatCpnt'
-
 import './sass/index.scss'
-
-declare global {
-    interface Window {
-        LPHelpers: {
-            addQueryOnLinks: Function;
-            chatCpnt: Function;
-        };
-    }
-}
 
 console.table(process.env)
 
-window.LPHelpers = {
-    addQueryOnLinks: AddQueryOnLinks,
-    chatCpnt: ChatCpnt
-}
+
+
